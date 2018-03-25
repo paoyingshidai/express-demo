7e24c805af96d69cd69d535bb847032a725521b7
Michael <13432849094@163.com>
2018-03-25 20:52:56 +0800

    解决问题：Can't set headers after they are sent. 问题是毁掉函数是异步执行的，下面的程序不会英文异步函数的执行而被阻塞。所以会多次调用 res.render().

--- routes/login.js
+++ routes/login.js
@@ -31,14 +31,17 @@ router.get('/login', function(req, res, next) {
             if (user != null && user.username === username) {
                 console.log("-----------------");
                 res.render('loginresult', { msg: 'login failed : for username has been registed ' });
+            } else {
+                var user = new User({username:username, password:password});
+                user.save();
+                res.render('loginresult', { msg: 'loginsuccess' });
             }
-            var user = new User({username:username, password:password});
-            user.save();
-            res.render('loginresult', { msg: 'loginsuccess' });
+            return ;
         } else {
             console.log(err);
         }
-    })
+    });
+    // 注意这里不能重复 res.render(); 否则会报错，因为query.exec 的毁掉函数是异步执行的两个程序；
 });
 
 module.exports = router;
